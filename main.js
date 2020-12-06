0a9ef4298d5437075993f6c3ed2626e8bd9224d3
jaefunk <jaefunk@me.com>
2020-12-06 20:58:38 +0900

    work web

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 
 const express = require('express');
-const app = express();
+
 const port = process.env.PORT || 8000;
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('hello, world!');
+})
+
+
+// var com = morgan('combined', {
+//     skip: function (req, res) { return res.statusCode < 400 }
+//   });
+// app.use(com);
+
 
 
 const webgl = require('./routers/webgl');
 app.use('/webgl', webgl);
 
-
-
 app.listen(port, () => {
     console.log('server is listening at localhost:', port);
 });
+
--- routers/webgl.js
+++ routers/webgl.js
@@ -1,10 +1,10 @@
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
-// router.use((req, res, next) => {
-//     next();
-// });
+
 router.get('/', (req, res) => {
-    res.send("a");
+    res.writeHead(200,{"Content-Type":"text/html"}); // 웹페이지 출력 
+    fs.createReadStream("./index.html").pipe(res); // 같은 디렉토리에 있는 index.html를 response 함 
 });
 
 router.get('/b', (req, res) => {
